ced45a49e0f142a3a11d02395a85f6a3080dc548
Nemias <nem.daz@gmail.com>
2016-07-21 14:36:28 -0500

    Detect actives controllers and functions

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -7,3 +7,25 @@ if ( ! function_exists('asset_url()'))
     return base_url().'assets/';
   }
 }
+
+if ( ! function_exists('active_link_controller'))
+{
+    function active_link_controller($controller)
+    {
+        $CI    =& get_instance();
+        $class = $CI->router->fetch_class();
+
+        return ($class == $controller) ? 'active' : NULL;
+    }
+}
+
+if ( ! function_exists('active_link_function'))
+{
+    function active_link_function($controller)
+    {
+        $CI    =& get_instance();
+        $class = $CI->router->fetch_method();
+
+        return ($class == $controller) ? 'active' : NULL;
+    }
+}
